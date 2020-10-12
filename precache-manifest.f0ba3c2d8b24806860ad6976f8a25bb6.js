c1b81df2b944a1f88c877cc7bcc242846fe5c19b
Derek Kim <ddkim1024@gmail.com>
2020-10-12 17:40:26 -0500

    Updates

---
+++ precache-manifest.f0ba3c2d8b24806860ad6976f8a25bb6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7cd53ede4577556ea963404f0006942f",
+    "url": "/shopping-cart/index.html"
+  },
+  {
+    "revision": "b298d8edf27253c85235",
+    "url": "/shopping-cart/static/css/main.feb908ba.chunk.css"
+  },
+  {
+    "revision": "d0fb92f0becdae0fd648",
+    "url": "/shopping-cart/static/js/2.b39ce8e6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/shopping-cart/static/js/2.b39ce8e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b298d8edf27253c85235",
+    "url": "/shopping-cart/static/js/main.2e31249a.chunk.js"
+  },
+  {
+    "revision": "ba80342531975976b290",
+    "url": "/shopping-cart/static/js/runtime-main.6e52efc5.js"
+  },
+  {
+    "revision": "d2ef2e97986a3baa159d4fe30edd6d05",
+    "url": "/shopping-cart/static/media/Minus.d2ef2e97.svg"
+  },
+  {
+    "revision": "cfa0b9df05b4c3bb628cbe602ab32574",
+    "url": "/shopping-cart/static/media/Plus.cfa0b9df.svg"
+  }
+]);
